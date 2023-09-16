014248627592bbd0507907a9d8f863211a3898a4
MarcelFeo <marcelfeo29@gmail.com>
2023-09-16 17:45:15 -0300

    adicionando base do trabalho

---
+++ MarcelFeoELazaroJoabe.hs
@@ -0,0 +1,41 @@
+import System.Random
+import System.IO
+
+jogarNovamente :: IO ()
+jogarNovamente = do
+    putStrLn "Você quer jogar novamente? (s ou n)"
+    op <- getLine
+
+    if op == "s"
+        then do
+            putStrLn "<< JOGO NOVO >>"
+            jogar
+        else
+            putStrLn "Até a próxima :)"
+
+verificaNum :: Int -> Int -> IO ()
+verificaNum numAleatorio qtdTentativas = do
+    putStr "Qual número você acha é? "
+    num <- readLn :: IO Int
+
+    if num == numAleatorio
+        then do
+            putStrLn $ "Você acertou!\nNúmero correto: " ++ show num ++ "\nTotal de tentativas: " ++ show qtdTentativas
+            jogarNovamente
+        else do
+            if num < numAleatorio
+                then putStrLn $ "Você errou. O número é maior."
+                else putStrLn $ "Você errou. O número é menor."
+            verificaNum numAleatorio (qtdTentativas + 1)
+
+jogar :: IO ()
+jogar = do
+    numAleatorio <- randomRIO (1, 100)
+    putStrLn "Digite um número entre 1 e 100:"
+    verificaNum numAleatorio 1
+
+main :: IO ()
+main = do
+    hSetBuffering stdout NoBuffering
+    putStrLn "<< JOGO DA ADIVINHAÇÃO >>"
+    jogar
